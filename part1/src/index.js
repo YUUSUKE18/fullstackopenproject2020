d60cb6f20765732ecf89f9f5a0a43f09935fa48e
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-03-26 22:44:35 +0900

    Completed Part1-C

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,65 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
+// const Header = (props) => {
+//   return(
+//     <div>
+//       <h1>{props.course}</h1>
+//     </div>
+//   )
+// }
+
+// const Part = (props) => {
+//   return(
+//     <div>{props.part} {props.excercise}</div>
+//   )
+// }
+
+// const Content = () => {
+//   return(
+//   <div>
+//       <Part part="Hello World" excercise="10"/>
+//       <Part part="Hello Morning" excercise="7" />
+//       <Part part="Hello Evening" excercise="5" />
+//   </div>
+//   )
+// }
+
+// const Total = () => {
+//   const excercise1 = 10;
+//   const excercise2 = 7;
+//   const excercise3 = 5;
+//   return(
+//     <div>
+//       <p>Number of excercise {excercise1 + excercise2 + excercise3 }</p>
+//     </div>
+//   )
+// }
+// const App = (props) => {
+//   const course ={
+//     name: "Half Stack Application Development",
+//     parts: [
+//       {
+//       name: 'Hello World',
+//       excercise: 10
+//       },
+//       {
+//       name: 'Hello Morning',
+//       excercise: 7
+//       },
+//       {
+//       name: 'Hello Evening',
+//       excercise: 14
+//       }
+//     ]
+//   }
+//   return(
+//   <div>
+//     <Header course={course} />
+//     <Content parts={parts}/>
+//     <Total parts={parts}/>
+//   </div>
+//   )
+// }
+
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
   )
 }
 
-const Part = (props) => {
+const Button = (props) => {
   return(
-    <div>{props.part} {props.excercise}</div>
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
-const Content = () => {
-  return(
-  <div>
-      <Part part="Hello World" excercise="10"/>
-      <Part part="Hello Morning" excercise="7" />
-      <Part part="Hello Evening" excercise="5" />
-  </div>
-  )
-}
 
-const Total = () => {
-  const excercise1 = 10;
-  const excercise2 = 7;
-  const excercise3 = 5;
-  return(
-    <div>
-      <p>Number of excercise {excercise1 + excercise2 + excercise3 }</p>
-    </div>
-  )
-}
+
 const App = (props) => {
-  const course ={
-    name: "Half Stack Application Development",
-    parts: [
-      {
-      name: 'Hello World',
-      excercise: 10
-      },
-      {
-      name: 'Hello Morning',
-      excercise: 7
-      },
-      {
-      name: 'Hello Evening',
-      excercise: 14
-      }
-    ]
-  }
-  return(
-  <div>
-    <Header course={course} />
-    <Content parts={parts}/>
-    <Total parts={parts}/>
-  </div>
+  const [counter, setCounter] = useState(0);
+
+  const Increment = () => setCounter(counter + 1);
+  const Decrement = () => setCounter(counter -1);
+  const Reset = () => setCounter(0);
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <br />
+    <Button
+      handleClick={Increment}
+      text="+"
+      />
+    <Button
+      handleClick={Decrement}
+      text="-"
+      />
+    <Button
+      handleClick={Reset}
+      text="Reset"
+      />
+    </div>
   )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
