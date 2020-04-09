d589088dd3b3ac0001fc3d4be488700a24a442fd
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-04-10 01:16:51 +0900

    Build React App Part2

---
+++ part2-exercise/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  const course = {
+    id:1,
+    name: 'Half Stack Application Development',
+    parts:[
+      {
+        name: 'Fundamental of React',
+        exercise: 10,
+        id:1
+      },
+      {
+        name: 'Middle Level of React',
+        exercise: 18,
+        id:2
+      },
+      {
+        name: 'Expert of React',
+        exercise: 12,
+        id:3
+      }
+    ]
+  }
+
+  return(
+    <div><Course course={course} /></div>
+  )
+}
+
+export default App;
