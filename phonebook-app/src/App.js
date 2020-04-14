41facc80d9f776fa63b361c304a959415b63eb6e
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-04-14 09:16:15 +0900

    Change Comp

--- phonebook-app/src/App.js
+++ phonebook-app/src/App.js
@@ -9,18 +9,38 @@ const App = () => {
       }
     ]
   )
+  const [newName, setNewName] = useState('');
+  
+  const onChangeName = (e) => {
+    e.target.value;
+    
+  }
+
+  const addName = (e) => {
+      e.preventDefault();
+      const addPhoneBook = {
+        name: newName,
+      }
+
+      setPerson(person.concat(addPhoneBook));
+      setNewName('');
+  }
+
   return (
     <div className="App">
       <h2>PhoneBook</h2>
       <form>
         <div>
-          name:  <input />
+          name:  <input value={onChangeName} />
         </div>
         <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   );
 }
