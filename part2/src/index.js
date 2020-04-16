5d9ba55b7b5d0b606d1590fd2004392043df3242
yuusuke <yuusuke.o.work.1018@gmail.com>
2020-04-16 22:05:53 +0900

    change package.json

--- part2/src/index.js
+++ part2/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes');
+console.log(promise);
+const promise2 = axios.get('http://localhost:3001/foobar');
+console.log(promise2);
 
 const notes = [
   {
